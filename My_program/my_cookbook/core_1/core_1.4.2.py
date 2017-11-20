5b1b517ce4245452dd83ccc4f4b77c121f86a3fd
zhangyuqing <2272672549@qq.com>
2017-11-20 11:37:26 +0800

    heapq库：heapq - 堆队列算法
    1.heapq.nlargest(n, iterable, key=None)
    返回可迭代定义的数据集中n最大元素的列表。
    
    2.heapq.nsmallest(n, iterable, key=None)
    返回可迭代定义的数据集中n最小元素的列表。

--- My_program/my_cookbook/core_1/core_1.4.2.py
+++ My_program/my_cookbook/core_1/core_1.4.2.py
@@ -5,3 +5,19 @@ print(heapq.nlargest(3,nums))   #print [42,37,23]
 print(heapq.nsmallest(3,nums))  #print() [-4,1,2]
 
 #测试是否正确提交到github
+
+portfolio = [
+   {'name': 'IBM', 'shares': 100, 'price': 91.1},
+   {'name': 'AAPL', 'shares': 50, 'price': 543.22},
+   {'name': 'FB', 'shares': 200, 'price': 21.09},
+   {'name': 'HPQ', 'shares': 35, 'price': 31.75},
+   {'name': 'YHOO', 'shares': 45, 'price': 16.35},
+   {'name': 'ACME', 'shares': 75, 'price': 115.65}
+]
+
+#返回的是整个列表非列表中的元素
+cheap = heapq.nsmallest(3,portfolio,key=lambda s:s['price'])
+expensive = heapq.nlargest(3,portfolio,key=lambda s:s['price'])
+
+print(cheap)
+print(expensive)
